febae186cad893570d740bf68784b4292809793c
JoePeterson51 <>
2021-06-01 09:45:18 -0600

    Start user story 1

---
+++ spec/features/studios/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Studio, type: :feature do
+  describe '#show' do
+    before :each do
+      @studio_1 = Studio.create!(name: "Universal", location: "Hollywood")
+      @studio_1.movies.create!(title: "The Goonies", creation_year: 1985, genre: "Adventure")
+      @studio_1.movies.create!(title: "The Big Lebowski", creation_year: 1998, genre: "Comedy")
+    end
+
+    it 'shows the studios name, location, and its movies' do
+      visit "/studios/#{@studio_1.id}"
+
+      expect(page).to have_content("Universal")
+      expect(page).to have_content("Hollywood")
+    end
+  end
+end
