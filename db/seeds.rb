96740f78126dbc70d8470e675f0a67144377f228
JoePeterson51 <>
2021-06-01 10:20:23 -0600

    Finish actor movies relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 studio_1 = Studio.create!(name: "Universal", location: "Hollywood")
-studio_1.movies.create!(title: "The Goonies", creation_year: 1985, genre: "Adventure")
-studio_1.movies.create!(title: "The Big Lebowski", creation_year: 1998, genre: "Comedy")
+movie_1 = studio_1.movies.create!(title: "The Goonies", creation_year: 1985, genre: "Adventure")
+movie_2 = studio_1.movies.create!(title: "The Big Lebowski", creation_year: 1998, genre: "Comedy")
+
 
